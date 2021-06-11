288fe310fae44452b8fab2662eed38c55e7fef10
Miyagi <a93297@alunos.uminho.pt>
2021-06-11 17:03:48 +0100

    pending_processing_done_killed com sinais

--- grupo-61 (Linux Intel)/src/aurras.c
+++ grupo-61 (Linux Intel)/src/aurras.c
@@ -5,11 +5,15 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <signal.h>
+typedef void (*sighandler_t)(int);
 
 char* pointerToString(int argc, char** argv){
     char*buf = malloc(1024);
     int size = 1024;
+    strcpy(buf,"");
+    pid_t pid = getpid();
+    sprintf(buf,"%d ",pid); 
     for(int i = 1; i < argc ; i++){
         strcat(buf,argv[i]);
         strcat(buf," ");
@@ -24,11 +28,46 @@ char* pointerToString(int argc, char** argv){
     return buf;
 }
 
+
+char* str;
+pid_t pid;
+
+void handler(int s){
+    if(s == SIGUSR1){
+        printf("Processing...\n");
+    }
+    else if( s == SIGUSR2 || s == SIGINT){
+        int file;
+        char str_aux[1024];
+        if(s == SIGUSR2)
+            printf("Done\n");
+        else
+            printf("Error\n");
+
+        if( (file = open("tmp/fifo",O_WRONLY)) == -1){
+            perror("Erro a abrir FIFO");
+        }
+        strcpy(str_aux,"");
+        strcat(str_aux,"used ");
+        strcat(str_aux,str);
+        
+        write(file,str_aux,strlen(str_aux));
+        free(str);
+        close(file);
+        kill(pid,SIGKILL);
+    }
+}
+
 int main(int argc, char** argv){
     int file;
     char buf[1024];
     int n=0;
-    
+    pid = getpid();
+
+    // --------- sinais
+    signal(SIGUSR1,handler);
+    signal(SIGUSR2,handler);
+    signal(SIGINT,handler);
     // --------- info
 
     if(argc == 1){
@@ -57,33 +96,47 @@ int main(int argc, char** argv){
     // ---------- transform
 
     else if( argc > 4 && !strcmp(argv[1],"transform")){
-    
-        if( (file = open("tmp/fifo",O_WRONLY)) == 1){
+        printf("Pending\n");
+        
+        if( (file = open("tmp/fifo",O_WRONLY)) == -1){
             perror("Erro a abrir FIFO");
             return -1;
         }
         
-        char* str;
         if((str = pointerToString(argc,argv))==NULL){
             return -1;
         }
         
         write(file,str,strlen(str));
-        free(str);
         close(file);
-        
-        if( (file = open("tmp/process",O_RDONLY)) == -1){
+
+    // ------- P|P|D/E ----------- 
+     /*   if( (file = open("tmp/process",O_RDONLY)) == -1){
             perror("Erro a abrir FIFO_PROCESS");
             return -1;
         }
     
-      // ------- P|P|D/E -----------
         while(n = read(file,buf,1024))    
             write(STDOUT_FILENO,buf,n);
-
         close(file);
+      
+      // ------- diminuir used -----
+        if( (file = open("tmp/fifo",O_WRONLY)) == -1){
+            perror("Erro a abrir FIFO");
+            return -1;
+        }
+        argv[1]="used";
+        char* str2;
+        if((str2 = pointerToString(argc,argv))==NULL){
+            return -1;
+        }        
+        write(file,str2,strlen(str));
+        free(str2);
+        close(file);
+        return 0;*/
+        pause();
+        pause();
     }
-    
     else{
         printf("Input inválido\n");
     }
--- grupo-61 (Linux Intel)/src/aurrasd.c
+++ grupo-61 (Linux Intel)/src/aurrasd.c
@@ -9,12 +9,10 @@
 #include <signal.h>
 typedef void (*sighandler_t)(int);
 
-#define N_Filters 5
 
 char *configs[100][4];
-char* task_name[100];
 int task;
-int c_tasks[100];
+int n_filters;
 char* filters;
 int used[100];
 
@@ -65,18 +63,24 @@ int redir(char* args[]){
 }
 
 int checkFiltros(char *args[], int n_args){
-    int i,idx;
+    int i,idx;//process;
     for(i = 3; i <= (n_args-2) ; i++){
-        for(idx=0; idx < N_Filters && strcmp(args[i],configs[idx][0])!=0 ; idx++);
-        if(idx == N_Filters)
-            return -1;
+        for(idx=0; idx < n_filters && strcmp(args[i],configs[idx][0])!=0 ; idx++);
+        if(idx == n_filters){
+         //   process = open("tmp/process",O_WRONLY,0644);
+         //   write(process,"Filtros inválidos\n",19);
+         //   close(process)
+            kill(atoi(args[0]),SIGINT);
+            return 0;
+        }
+            
     }
-    return 0;
+    return 1;
 }
 
 void sendStatus(int fifo){
     char* buf = malloc(1024);
-    for(int i = 0; i < N_Filters ; i++){
+    for(int i = 0; i < n_filters ; i++){
         write(fifo,"filter ",8);
         write(fifo,configs[i][0],strlen(configs[i][0]));        
         sprintf(buf,": %d/%s (running/max)\n",used[i],configs[i][2]);
@@ -85,6 +89,7 @@ void sendStatus(int fifo){
     sprintf(buf,"pid: %d\n",getpid());
     write(fifo,buf,strlen(buf));
 }
+
 // t in out f1 f2 f3
 void muda_Used(char* args[], int n_args, int s_d){
     int j;
@@ -107,13 +112,13 @@ int main(int argc , char* argv[]){
     mkfifo("tmp/status",0644); // rw-r--r--
     chmod("tmp/fifo",0622); // rw--w--w-
     chmod("tmp/status",0644); // rw-r--r--
-    mkfifo("tmp/process",0644); // rw-r--r--
-    chmod("tmp/process",0644); // rw-r--r--
+  //  mkfifo("tmp/process",0644); // rw-r--r--
+  //  chmod("tmp/process",0644); // rw-r--r--
 
 // ----------- inicializador de ficheiros e verificação
     task=0;
     char *buf= malloc(1024);
-    int config, fifo=-1, status, n,process;
+    int config, fifo=-1, status, n;
     
     if( argv[1] ){
         if ((config = open(argv[1],O_RDONLY,0622)) == -1){
@@ -134,20 +139,27 @@ int main(int argc , char* argv[]){
     
     for(int i = 0; i < 100 ; i++){
         used[i]=0;
-        c_tasks[i]=-1;
     }
 
 // ------------ inicializador do array de configuração
     
     read(config,buf,1024);
+    n_filters=0;
+    int enter_aux;
+    for(enter_aux = 0; buf[enter_aux] ; enter_aux++){
+        if(buf[enter_aux]=='\n')
+            n_filters++;
+    }
+    if(buf[enter_aux-1] != '\n')
+        n_filters++;
 
-    for(int j = 0; j < N_Filters ; j++){
+    for(int j = 0; j < n_filters ; j++){
         for(int i = 0; i < 3 ; i++)
             configs[j][i] = strdup(strsep(&buf," \n"));
 
         configs[j][3] = NULL;
     }
-    free(buf);
+    //free(buf);
     buf=malloc(1024);
     
 //  ---------Abertura do FIFO
@@ -157,65 +169,63 @@ int main(int argc , char* argv[]){
              return -1;
         }
         task++;
-        if(!fork()){
-            while((n = read(fifo,buf,1024))){
-                if(!strcmp(buf,"status")){
-                    status = open("tmp/status",O_WRONLY,0644);
-                    sendStatus(status);
-                    close(status);
+        while((n = read(fifo,buf,1024))){
+        // ---- divisao do buf em palavras ----
+            char *exec_args[100];
+            int word = 1;
+            exec_args[0] = buf;
+            for (int i = 0; buf[i]; i++){
+                if (buf[i] == ' '){
+                    buf[i] = '\0';
+                    exec_args[word++] = buf + i + 1;
                 }
-                else{
-                // ---- divisao do buf em palavras ----
-                    char *exec_args[100];
-                    int word = 1;
-                    exec_args[0] = buf;
-                    for (int i = 0; buf[i]; i++){
-                        if (buf[i] == ' '){
-                            buf[i] = '\0';
-                            exec_args[word++] = buf + i + 1;
-                        }
-                    }
-                    exec_args[word] = NULL;
+            }
+            exec_args[word] = NULL;
+            word--;
+            if(!strcmp(buf,"status")){
+                status = open("tmp/status",O_WRONLY,0644);
+                sendStatus(status);
+                close(status);
+            }
+            else if(!strcmp(exec_args[0],"used")){
+                muda_Used(&exec_args[2],word-1,-1);
+            }            
+            else if(checkFiltros(&exec_args[1],word)){                
+            //-----------------------------------------
+            //    process = open("tmp/process",O_WRONLY,0644);
+            //    write(process,"Pending\n",9);
 
+            //    checkFree(exec_args,word);
+
+            //-------------Paragem --------------------    
+                kill(atoi(exec_args[0]),SIGUSR1);
+            //    write(process,"Processing...\n",15);
+                muda_Used(&exec_args[1],word,1);
+                if(!fork()){
                 //------ redirecionamento ----------------
-
-                    if(redir(exec_args)==-1){
+                    if(redir(&exec_args[1])==-1){
                         return -1;
                     }
-        
-                //-----------------------------------------
-                    process = open("tmp/process",O_WRONLY,0644);
-                    write(process,"Pending\n",9);
-
-                //-------------Paragem --------------------    
-
-                    write(process,"Processing...\n",15);
-
-                    if(checkFiltros(exec_args,word)==-1){
-                        write(process,"Error\n",7);
-                        close(process);
-                        _exit(-1);
-                    }
-
-                    muda_Used(exec_args,word,1);
 
                     pid_t pid_filho = fork();
                     int ret;
                     if(!pid_filho){
-                        _exit(aplicaFiltros(exec_args,word));
+                        _exit(aplicaFiltros(&exec_args[1],word));
                     }
                     else{                       
                         waitpid(pid_filho,&ret,0);
-                        muda_Used(exec_args,word,-1);
                         if(WEXITSTATUS(ret)==-1)
-                            write(process,"Error\n",7);
+                            kill(atoi(exec_args[0]),SIGINT);
+                            //write(process,"Error\n",7);
                         else
-                            write(process,"Done\n",6);
-                        close(process);
+                            kill(atoi(exec_args[0]),SIGUSR2);
+                            //write(process,"Done\n",6);
                     }
+                    _exit(0);
                 }
-            }
-            _exit(0);
+              //  close(process);
+                
+            }    
         }
     }
 
